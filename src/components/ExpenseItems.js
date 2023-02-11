7441b0dea2c14e1ae67fad89b68059f9186e6019
Abhishekkanojiya3 <abhi67562@gmail.com>
2023-02-12 02:21:41 +0530

    first commit

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,13 @@
+function ExpenseItems() {
+    return ( <
+        div >
+        <
+        h2 > Expense items! < /h2> <
+        h3 > FOOD RS 200 < /h3> <
+        h3 > Petrol Rs 100 < /h3> <
+        h3 > Movies Rs 200 < /h3> <
+        /div>
+    );
+}
+
+export default ExpenseItems;
