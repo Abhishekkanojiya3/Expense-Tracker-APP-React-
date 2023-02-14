5ccda924f30f62f0d4825285b72edfbaa62d652b
Abhishekkanojiya3 <abhi67562@gmail.com>
2023-02-15 01:56:08 +0530

    Chnge Expense

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,21 +1,28 @@
+import React, { useState } from 'react';
 import './ExpenseItems.css';
 import Card from '../UI/Card';
 
 const ExpenseDetails = (props) => {
-    const deleteExpense = () => {
-        console.log(typeof props.onDeleteExpense); // add this line to check the type of onDeleteExpense prop
-        props.onDeleteExpense(props.id);
+    const [title, setTitle] = useState(props.title);
+    const [amount, setAmount] = useState(props.amount);
+    const changeExpense = () => {
+        setAmount('$100')
+        console.log(amount);
+    }
+    const clickHandler = () => {
+        setTitle('update');
+        console.log(title);
     }
     return (
 
         <
         Card className = 'expense-item__description' >
         <
-        h2 > { props.title } < /h2>  <
+        h2 > { title } < /h2>  <
         h2 > { props.location } < /h2>  <
-        div className = 'expense-item__price' > $ { props.amount } < /div>   <
-        button > Change Item < /button> <
-        button onClick = { deleteExpense } > Delete Expense < /button> <
+        div className = 'expense-item__price' > $ { amount } < /div>   <
+        button onClick = { clickHandler } > Change Item < /button> <
+        button onClick = { changeExpense } > Change Expense < /button> <
         /Card>
     );
 }
