edeaf73e40f5a209bbe113967e18cbdabbc1a659
Abhishekkanojiya3 <abhi67562@gmail.com>
2023-02-18 01:32:53 +0530

    assignment

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
 const NewExpense = (props) => {
-    const newExpenseHandler = (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            id: Math.random().toString()
-        };
-        props.onAddExpense(expenseData);
-    }
+        const [isEditing, setEditing] = useState(false);
+        const newExpenseHandler = (enteredExpenseData) => {
+            const expenseData = {
+                ...enteredExpenseData,
+                id: Math.random().toString()
+            };
+            props.onAddExpense(expenseData);
+            setEditing(false);
+        }
+        const startEditHandler = () => {
+            setEditing(true);
+        }
+        const stopEditHandler = () => {
+            setEditing(false);
+        }
 
-    return ( <
-        div className = "new-expense" >
-        <
-        ExpenseForm onSaveData = { newExpenseHandler }
-        /> <
-        /div>
-    );
+        return ( <
+                div className = "new-expense" > {!isEditing && ( < button onClick = { startEditHandler } > Add New Expense < /button>)} {
+                            isEditing && ( < ExpenseForm onSaveData = { newExpenseHandler }
+                                onCancel = { stopEditHandler }
+                                />)} <
+                                /div>
+                            );
 
-};
+                        };
 
-export default NewExpense;
+                        export default NewExpense;
